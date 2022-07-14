31451b3b1d2aa1d6559a68da8204f59735e7a810
Marco Fronzi <marco.fronzi@gmail.com>
2022-07-14 20:02:20 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='bnn',  
+     version='0.1',
+     scripts=['bnn'] ,
+     author="Marco Fronzi",
+     author_email="marco.fronzi@.com",
+     description="UNDER CONSTRUCTION",
+    url='https://github.com/fronzi/projBNN/tree/master/BNN',
+    keywords=['Machine Learning'],
+    install_requires=['requirements.txt'],
+
+),
+ )
